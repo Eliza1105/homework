d869a0a9e3e9a083cef0202582a1821b8607e0d8
14IIL05 <lelizaveta776@gmail.com>
2024-04-06 15:37:16 +0300

    hello

--- Homework/HW_Prilozheniye_5.java
+++ Homework/HW_Prilozheniye_5.java
@@ -12,47 +12,43 @@ import java.util.List;
 public class HW_Prilozheniye_5 {
     public static void main(String[] args) {
         List<StoreGoods> storeGoods = new ArrayList<>();
-        storeGoods.add(new Cabinets("Стефани",254674,249,"Купе"));
-        storeGoods.add(new Cabinets("Верона",467459,315,"Обычная"));
-        storeGoods.add(new Tables("Кельн",466939,199,3));
-        storeGoods.add(new Tables("Токио",204958,176,5));
-        storeGoods.add(new Chairs("Гарден",946321,83,true));
-        storeGoods.add(new Chairs("Вита",798651,69,false));
+        storeGoods.add(new Cabinets("Стефани","254674",249,"Купе"));
+        storeGoods.add(new Cabinets("Верона","467459",315,"Обычная"));
+        storeGoods.add(new Tables("Кельн","466939",199,3));
+        storeGoods.add(new Tables("Токио","204958",176,5));
+        storeGoods.add(new Chairs("Гарден","946321",83,true));
+        storeGoods.add(new Chairs("Вита","798651",69,false));
 
         List<StoreGoods> storeSet1 = new ArrayList<>();
-        storeSet1.add(new Tables("Кельн",466939,199,3));
-        storeSet1.add(new Chairs("Гарден",946321,83,true));
-        storeSet1.add(new Chairs("Гарден",946321,83,true));
-        storeGoods.add(new Sets("Набор 1",576321, 365));
+        storeSet1.add(new Tables("Кельн","466939",199,3));
+        storeSet1.add(new Chairs("Гарден","946321",83,true));
+        storeSet1.add(new Chairs("Гарден","946321",83,true));
+        storeGoods.add(new Sets("Набор 1","576321", storeSet1));
 
-        Collections.sort(storeGoods,Collections.reverseOrder());
+        Collections.sort(storeGoods,(f1,f2)->Double.compare(f1.getPrice(),f2.getPrice()));
 
-        System.out.println(storeGoods);
 
-// Collections.sort(furnitureItems, (f1, f2) -> Double.compare(f1.getPrice(), f2.getPrice())); // Сортировка по цене
-//
-//        for (FurnitureItem item : furnitureItems) {
-//            System.out.println(item);
-//        }
+     for (StoreGoods good : storeGoods) {
+            System.out.println(storeGoods);
+     }
     }
 }
 
 interface Saleman{
-    String saleThings();
+    String toString();
 }
 abstract class StoreGoods implements Saleman{  // в некоторых источниках рекомендуют сделать класс абстрактным
     String category;
     String name;
-    int vendorCode;
-    int price;
+    String vendorCode;
+     double price;
 
-    public StoreGoods(String category, String name, int vendorCode, int price) {
+    public StoreGoods(String category, String name, String vendorCode, double price) {
         this.category = category;
         this.name = name;
         this.vendorCode = vendorCode;
         this.price = price;
     }
-
     public String getCategory() {
         return category;
     }
@@ -61,71 +57,76 @@ abstract class StoreGoods implements Saleman{  // в некоторых исто
         return name;
     }
 
-    public int getVendorCode() {
+    public String getVendorCode() {
         return vendorCode;
     }
 
-    public int getPrice() {
+    public double getPrice(){
         return price;
+    }
+    @Override
+    public String toString() {
+        return category + ":" + name + ", (Арт. " + vendorCode + " ), цена:  " + price;
     }
 }
 class Cabinets extends StoreGoods{  //Класс для шкафов
     private String doorType;
-    public Cabinets( String name, int vendorCode, int price, String doorType) {
+    public Cabinets( String name, String vendorCode, double price, String doorType) {
         super("Шкафы", name, vendorCode, price);
         this.doorType = doorType;
     }
-    public String getDoorType() {
-        return doorType;
-    }
     @Override
-    public String saleThings() {
-        return super.toString()+" , тип дверей: "+getDoorType();
+    public String toString() {
+        return super.toString()+" , тип дверей: "+doorType;
     }
 }
 class Tables extends StoreGoods{  // Класс для столов
     private int sufliadka;
 
-    public Tables(String name, int vendorCode, int price, int sufliadka) {
+    public Tables(String name, String vendorCode, double price, int sufliadka) {
         super("Столы", name, vendorCode, price);
         this.sufliadka = sufliadka;
     }
-    public int getSufliadka() {
-        return sufliadka;
-    }
     @Override
-    public String saleThings() {
-        return super.toString()+" ,количество выдвижных ящиков: "+ getSufliadka();
+    public String toString() {
+        return super.toString()+" ,количество выдвижных ящиков: "+ sufliadka;
     }
 }
 
 class Chairs extends StoreGoods{
     private boolean hasBackrest;
 
-    public Chairs(String name, int vendorCode, int price, boolean chairBack) {
+    public Chairs(String name, String vendorCode, double price, boolean chairBack) {
         super("Стулья", name, vendorCode, price);
         this.hasBackrest = chairBack;
     }
     @Override
-    public String saleThings() {
+    public String toString() {
         return super.toString()+" ,наличие спинки:  "+(hasBackrest?"есть":"нет");
     }
 }
 
 class Sets extends StoreGoods{
-    private List<StoreGoods> goods= new ArrayList<>();
+    private List<StoreGoods> goods;
 
-    public Sets(String name, int vendorCode, int price) {
-        super("Наборы", name, vendorCode, price);
+    public Sets(String name, String vendorCode, List<StoreGoods> goods) {
+        super("Наборы", name, vendorCode, calculateSetPrice(goods));
         this.goods = goods;
     }
 
-    @Override
-    public int getPrice() {
-        return (int)(super.getPrice()*0.95);
+    private static double calculateSetPrice(List<StoreGoods> goods) {
+        double totalPrice = 0;
+        for (StoreGoods good : goods) {
+            totalPrice += good.getPrice();
+        }
+        return totalPrice * 0.95; // Скидка 5% для набора
     }
     @Override
-    public String saleThings() {
+    public double getPrice() {
+        return price;
+    }
+    @Override
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("").append(name).append(" Арт. ").append(vendorCode).append("\n");
         for (StoreGoods good : goods){

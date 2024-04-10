ba3bb43f3a9ac7c320f4104ac6ee3a3eaecfdad6
14IIL05 <lelizaveta776@gmail.com>
2024-04-10 23:10:52 +0300

    hello

--- Homework/HW_Generiks/HW_Generics_Pair.java
+++ Homework/HW_Generiks/HW_Generics_Pair.java
@@ -1,7 +1,12 @@
 package Homework.HW_Generiks;
 
 import java.util.Objects;
-
+//*Реализуйте generic-класс Pair, похожий на Optional, но содержащий пару элементов разных типов и не запрещающий элементам
+//принимать значение null.
+//*Реализуйте методы getFirst(), getSecond(), equals() и hashCode().
+//*Реализуйте статический фабричный метод of(). Конструктор должен быть закрытым (private).
+//*Реализуйте метод ifPresent, аналогичный одноименному методу класса Optional и принимающий java.util.function.BiConsumer
+//Реализуйте метод empty(), который возвращает экземпляр с нулевыми полями {null,null} (см. реализацию в классе Optional).
 public class HW_Generics_Pair {
         public static void main(String[] args) {
            // Pair<Integer, String> pair = new Pair<>(1,"2");
@@ -38,6 +43,12 @@ public class HW_Generics_Pair {
         public String toString() {
             return "(" + first + ", " + second + ")";
         }
+       /* public void ifPresent(BiConsumer<T, U> consumer) {
+            consumer.accept(first, second);
+         }
+
+        */
+        //не уверена в этом решении
 
         @Override
         public boolean equals(Object obj) {
@@ -45,6 +56,8 @@ public class HW_Generics_Pair {
             if (obj == null || getClass() != obj.getClass()) return false;
             Pair<?, ?> pair = (Pair<?, ?>) obj;
             return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+            //        if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            //        return second != null ? second.equals(pair.second) : pair.second == null;
         }
 
         @Override
